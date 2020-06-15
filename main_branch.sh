2025b2cb38070419dcf4528229cafc26c17d15cd
Dave Syer <dsyer@pivotal.io>
2020-06-15 14:34:04 +0000

    Add --base option in retarget

--- main_branch.sh
+++ main_branch.sh
@@ -7,11 +7,12 @@ if [ "$1" == "--help" ]; then
 
 Create a new branch called main if it doesn't exist and make it the deafult. Usage:
 
-$ main_branch.sh [--org org] [--repo repo] [branch]
+$ main_branch.sh [--org org] [--base base] [--repo repo] [branch]
 
 where
 
 * org is the Github organization (default user's own org)
+* base is the name of the existing base branch (default "master")
 * repo is the repo name (default all)
 * branch is the new branch name (default "main")
 
@@ -26,6 +27,13 @@ if [ "$1" == "--org" ]; then
 else
 	org=`hub api /user | jq -r '.login'`
 fi
+if [ "$1" == "--base" ]; then
+	base=$2
+	shift
+	shift
+else
+	base=master
+fi
 if [ "$1" == "--repo" ]; then
 	repo=$2
 	shift
@@ -35,10 +43,11 @@ branch=${1:-main}
 
 function main_branch {
 	org=$1
-	repo=$2
-	branch=$3
-	if ! hub api /repos/${org}/${repo}/branches | jq -r '.[].name' | grep -q master; then
-		echo No master branch in ${repo}
+	base=$2
+	repo=$3
+	branch=$4
+	if ! hub api /repos/${org}/${repo}/branches | jq -r '.[].name' | grep -q ${base}; then
+		echo No ${base} branch in ${repo}
 		return
 	fi
 	echo Processing ${repo}
@@ -47,14 +56,13 @@ function main_branch {
 	cd tmp
 	(git checkout -b ${branch}; git push origin ${branch} --set-upstream) || git checkout ${branch}
 	hub api /repos/${org}/${repo} --field name="${repo}" --field default_branch="${branch}" > /dev/null && echo Changed default branch
-	retarget.sh --org ${org} ${repo} ${branch}
-	git push origin :master
+	retarget.sh --org ${org} --base ${base} ${repo} ${branch} && git push origin :${base}
 }
 
 if [ -z ${repo} ]; then
 	for repo in `hub api --obey-ratelimit --paginate /users/${org}/repos | sed '/^\]/ {N; s/\]\n\[/,/g}' | jq -r '.[] | select(.fork!=true) | .name'`; do
-		main_branch ${org} ${repo} ${branch}
+		main_branch ${org} ${base} ${repo} ${branch}
 	done
 else
-	main_branch ${org} ${repo} ${branch}
+	main_branch ${org} ${base} ${repo} ${branch}
 fi
