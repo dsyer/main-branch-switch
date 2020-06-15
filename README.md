8791089a7eea88b453f58f5e7d0f3e58c096f29f
Dave Syer <dsyer@pivotal.io>
2020-06-15 11:05:50 +0000

    Only retarget PRs that have the right target

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 Scripts for switching Github repos to use a "main" branch as the default.
 
-Pre-requisites: you need the [Hub CLI](https://hub.github.com/) to interact with Github, and you need [JQ](https://stedolan.github.io/jq/) for parsing and mangling JSON. Make sure both the scripts in this repo are on your `PATH`.
+Pre-requisites: you need the [Hub CLI](https://hub.github.com/) to interact with Github, and you need [JQ](https://stedolan.github.io/jq/) for parsing and mangling JSON. Make sure both the scripts in this repo are on your `PATH`. You may need to authenticate with Github via Hub before you start (or it might prompt you the first time it needs it).
 
 To create a "main" branch and retarget all the pull requests in all the repositories in your personal Github organization:
 
@@ -9,3 +9,12 @@ $ main_branch.sh
 ```
 
 You can switch orgs `--org <myorg>` and you can target a specific repository with `--repo <myrepo>`.
+
+The other script can be used on its own to re-target pull requests (it is used by the `main_branch.sh` script):
+
+```
+$ retarget.sh --org <myorg> <myrepo> main 1234
+```
+
+The default org is your personal org, and the default pull request is all of them (the open ones).
+
--- retarget.sh
+++ retarget.sh
@@ -7,11 +7,12 @@ if [ $# -lt 2 ]; then
 
 Re-target pull request(s) onto a new branch. Usage:
 
-$ retarget.sh [--org org] repo branch [pr]
+$ retarget.sh [--org org] [--base base] repo branch [pr]
 
 where
 
 * org is the Github organization (default user's own org)
+* base is the name of the base branch to filter on (default "master")
 * repo is the repo name
 * branch is the new branch name
 * pr is the pull request number (default to all open)
@@ -27,22 +28,35 @@ if [ "$1" == "--org" ]; then
 else
 	org=`hub api /user | jq -r '.login'`
 fi
+if [ "$1" == "--base" ]; then
+	base=$2
+	shift
+	shift
+else
+	base=master
+fi
 repo=$1; shift
 branch=$1; shift
 pr=$1
 
 function retarget {
 	org=$1
-	repo=$2
-	branch=$3
-	pr=$4
-	hub api /repos/${org}/${repo}/pulls/${pr} --field base=$branch > /dev/null && echo Changed target branch of PR ${pr}
+	base=$2
+	repo=$3
+	branch=$4
+	pr=$5
+	actual=`hub api /repos/${org}/${repo}/pulls/${pr} | jq -r '.base.ref'`
+	if [ "${actual}" == "${base}" ]; then
+		hub api /repos/${org}/${repo}/pulls/${pr} --field base=$branch > /dev/null && echo Changed target branch of PR ${pr}
+	else
+		echo PR ${pr} is based on ${actual} \(not ${base}\)
+	fi
 }
 
 if [ -z ${pr} ]; then
 	for pr in `hub api --obey-ratelimit --paginate /repos/${org}/${repo}/pulls | sed '/^\]/ {N; s/\]\n\[/,/g}' | jq '.[].number'`; do
-		retarget ${org} ${repo} ${branch} ${pr}
+		retarget ${org} ${base} ${repo} ${branch} ${pr}
 	done
 else
-	retarget ${org} ${repo} ${branch} ${pr}
+	retarget ${org} ${base} ${repo} ${branch} ${pr}
 fi
